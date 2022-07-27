60693272bc4c0ee7b739d88b8ae4e6f4bf087506
GabrielLuizSF <gabrielluizads123@gmail.com>
2022-07-27 19:58:03 -0300

    functional test

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -2,8 +2,11 @@ import supertest from "supertest";
 
 describe('Beach forecast functional tests', () => {
   it('should return a forecast with just a few times', async() => {
+    
     const { body, status } = await global.testRequest.get('/forecast');
+
     expect(status).toBe(200);
+
     expect(body).toEqual([{
       "time": "2020-04-26T00:00:00+00:00",
       "forecast": [{
